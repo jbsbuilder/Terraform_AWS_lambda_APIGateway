6464e25e9b870ff10835dff61ffbf503e68f44bc
jbsbuilder <work.jacobsmith@gmail.com>
2024-02-02 18:26:48 -0700

    AWS setup code written in main.tf

--- main.tf
+++ main.tf
@@ -9,20 +9,63 @@ data "archive_file" "lambda_zip" {
 }
 
 resource "aws_iam_role" "lambda-iam" {
-  name = "lambda-iam"
+  name               = "lambda-iam"
   assume_role_policy = <<EOF
-    {
+{
     "Version": "2012-10-17",
     "Statement": [
-        {
+      {
         "Action": "sts:AssumeRole",
         "Principal": {
             "Service": "lambda.amazonaws.com"
         },
         "Effect": "Allow",
         "Sid": ""
-        }
+      }
     ]
-    }
-    EOF
+}
+
+  EOF
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename         = "lambda.zip"
+  function_name    = "lambda-fuction"
+  role             = aws_iam_role.lambda-iam.arn
+  handler          = "lambda.lambda_handler"
+  source_code_hash = data.archive_file.lambda-zip.output_base64sha256
+  runtime          = "python3.8"
+}
+
+resource "aws_apigatewayv2_api" "lambda-api" {
+  name          = "v2-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigateway2_stage" "lambda-stage" {
+  api_id      = aws_apigatewayv2_api.lambda-api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_apigateway_integration" "lambda-integration" {
+  api_id               = aws_apigatewayv2_api.lambda-api.id
+  integration_type     = "AWS_PROXY"
+  integration_method   = "POST"
+  integration_uri      = aws_lambda_function.lambda.invoke_arn
+  passthrough_behavior = "WHEN_NO_MATCH"
+}
+
+resource "aws_apigateway2_route" "lambda_route" {
+  api_id    = aws_apigatewayv2_api.lambda-api.id
+  route_key = "GET /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda-ingtegration.id}"
+}
+
+resource "aws_lambda_permission" "api-gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.lambda-api.execution_arn}/*/*/*"
 }
